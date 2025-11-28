4695dc362d8dbe217c132fc534e0ac0e247e346a
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:25:15 +0200

    ghhh

---
+++ KITKAT/KITKAT/KujundiKalkulaator/KujundiKalkulaator/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace KujundiKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Ruudu sees asub ring. ringi raadius on 3 ühikut
+            //leia ja väljesta ekraanile ruudu pindala ja ümbermõõt
+
+            int roundRadius = 3;
+            int diameter = roundRadius * 2;
+            double squareArea = diameter * diameter;
+            double squarePerimeter = 4 * diameter;
+            
+            Console.WriteLine("Ruudu pindala:" + squareArea);
+            Console.WriteLine("Ruudu ümbermõõt: " + squarePerimeter);
+
+            //arvutada ringi pindala ja ümbermõõt
+            //math.pi
+            double CircleC = 2 * Math.PI * roundRadius;
+
+            Console.WriteLine("Ringi ümbermõõt on " + CircleC);
+
+            double CircleS = Math.PI * (roundRadius * roundRadius);
+
+            Console.WriteLine("ringi Pindala on " + CircleS);
+        }
+    }
+}
---
+++ KITKAT/KITKAT/MaaKalkulaator/MaaKalkulaator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MaaKalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Meil on münt, mille diameeter on 25.75 mm
+            //Arvutada välja Maa ümbermõõt ja mitu münti
+            //Mahub järjestikku ümber Maa
+            //Arvutuses tohib kasutada ainult mündi diameetrit ja Maa raadiust
+            double coinDiameter = 25.75; //mm
+            double earthRadius = 6378100000;
+            double aroundEarth = 2 * Math.PI * earthRadius;
+            Console.WriteLine(aroundEarth);
+            double howManyCoins = aroundEarth / coinDiameter;
+            Console.WriteLine("Kahe euroseid läheb ümber maa {0:0}", howManyCoins);
+        }
+    }
+}
