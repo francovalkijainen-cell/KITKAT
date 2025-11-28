e7a25afbf0d872b24cb6fbda36c9bbfa57c97f24
Opilane TTHK <opilane@tthk.ee>
2025-11-28 11:43:09 +0200

    ghhh

---
+++ KITKAT/KITKAT/ComputerCakllogic/ComputerCakllogic/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ComputerCakllogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("kui palju on 3 + 8 / (4 - 2) * 4 = ?");
+
+            double calculation1 = 3 + 8 / (4 - 2) * 4;
+            float calculation2 = 3 + 8 / (4 - 2) * 4;
+            double calculation3 = 3 + 8 / (4 - 2) * 4;
+            double calculation4 = 3 + 8 / (4 - 2) * 4;
+
+            Console.WriteLine("3 + 8 / (4 - 2) * 4;" + calculation1);
+            Console.WriteLine("3 + 8 / (4 - 2) * 4;" + calculation2);
+            Console.WriteLine("3 + 8 / (4 - 2) * 4;" + calculation3);
+            Console.WriteLine("3 + 8 / (4 - 2) * 4;" + calculation3);
+            Console.WriteLine("3 + 8 / (4 - 2) * 4;" + calculation4);
+
+        }
+    }
+}
