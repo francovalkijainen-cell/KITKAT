03739ef7e74874dcaa617477012c7d6353777d50
Opilane TTHK <francovalkijainen@gmail.com>
2025-12-05 11:08:14 +0200

    ForLoopAdding

---
+++ KITKAT/KITKAT/ForLoopAdding/ForLoopAdding/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ForLoopAdding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Täisarvude kokku liitmine kuni kümneni");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("\n\n");
+
+            int j, sum = 0;
+
+            for (j = 1; j <= 10; j++)
+            {
+                sum = sum + j;
+                Console.WriteLine(" summa on: " + sum);
+            }
+
+        }
+    }   
+}
