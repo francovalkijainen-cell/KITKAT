a6781ca7aa5a93d660b7e78e41da6c7e6999b7ed
Opilane TTHK <francovalkijainen@gmail.com>
2025-12-05 11:32:35 +0200

    ForKillKoll

---
+++ KITKAT/KITKAT/ForKillKoll/ForKillKoll/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ForKillKoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("sisesta korduste arv");
+            int arv = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < arv; i++)
+            {
+                Console.WriteLine("kill-koll");
+            }
+            for (int k = 0; k < 1; k++)
+            {
+                Console.WriteLine(" killadi-koll");
+            }
+            Console.WriteLine("kill-koll");
+
+        }
+    } 
+}
+
+
+
+
+            
