e7c031c2e56e08ea84024748ffdd87dc0c32e638
Opilane TTHK <francovalkijainen@gmail.com>
2025-12-05 09:01:21 +0200

    kitkatggg

---
+++ KITKAT/KITKAT/ForLoopHello/ForLoopHello/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ForLoopHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Teretulemast");
+
+            //teha konsoolirakendus, kus kasutad for loopi
+            //kolm korda näidatakse tere tulemast
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Teretulemast");
+            }
+        }
+    }
+}
