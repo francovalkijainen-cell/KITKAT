1fe434edf13d30bcac44d84a168511eb85eaa2d5
Opilane TTHK <francovalkijainen@gmail.com>
2025-12-05 10:35:13 +0200

    kkkk

---
+++ KITKAT/KITKAT/ForAsterisk/ForAsterisk/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+namespace ForAsterisk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For Asteriks");
+
+            int nr = Convert.ToInt32(Console.ReadLine());
+
+            //tuleb kasutada for loopi
+            //sisestan ridade arvu ja tuleb vastav ridade arv konsooli
+            //peab naitama tärne
+            for (int i = 0; i < nr; i++)
+            {
+                if (i == i)
+                {
+                    Console.WriteLine("*");
+
+                }
+                else
+                {
+                    Console.WriteLine(" ");
+                }
+
+
+            }
+        }
+    }
+}
+
