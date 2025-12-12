0a84d086aacd54875d91f2c554c47ec63bd500bd
Opilane TTHK <opilane@tthk.ee>
2025-12-12 11:40:09 +0200

    kkk

--- KITKAT/KITKAT/MaaKalkulaator/ForLoopPyramid/ForLoopPyramid/Program.cs
+++ KITKAT/KITKAT/MaaKalkulaator/ForLoopPyramid/ForLoopPyramid/Program.cs
@@ -14,10 +14,20 @@
 
             for (i = 0; i < n; i++)
             {
-                for (j = 0; j <= n - i; j++)  
+                for (j = 0; j <= n - i; j++)
                     Console.Write(" ");
 
-                for (j = 1; j <= 2 * i - 1; j++)  
+                for (j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+
+                Console.Write("\n");
+            }
+            for (i = n - 1;i >= 1; i--)
+            {
+                for (j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+
+                for (j = 1; j <= 2 * i - 1; j++)
                     Console.Write("*");
 
                 Console.Write("\n");
