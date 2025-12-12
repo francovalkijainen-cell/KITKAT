ec28331533be08d6a6506c85577e788c242268d5
Opilane TTHK <opilane@tthk.ee>
2025-12-12 11:08:21 +0200

    triangle

---
+++ KITKAT/KITKAT/MaaKalkulaator/ForLoopTriangle/ForLoopTriangle/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ForLoopTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tärnidest kolmnurk e poolik püramiid");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("\n\n");
+            
+
+            Console.WriteLine("sisesta ridade arv: ");
+            int i,j,rows; 
+             
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            for ( i = 1; i <= rows; i++)
+            {
+                for ( j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
