1d9c9a968d45332f0ce3e13052f7a750a4c7e29d
Opilane TTHK <opilane@tthk.ee>
2025-12-12 11:22:00 +0200

    pyramid

---
+++ KITKAT/KITKAT/MaaKalkulaator/ForLoopPyramid/ForLoopPyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ForLoopPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loome tärnidest püramiidi: \n");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("sisesta suurus: ");
+
+            int i, j, n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j <= n - i; j++)  
+                    Console.Write(" ");
+
+                for (j = 1; j <= 2 * i - 1; j++)  
+                    Console.Write("*");
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
