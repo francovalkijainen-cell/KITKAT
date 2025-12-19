133b4d4db5345ab69a6d625f22a9dff9c3cefdfd
Opilane TTHK <opilane@tthk.ee>
2025-12-19 11:21:24 +0200

    peaceyolobye

---
+++ KITKAT/KITKAT/ArrayString/ArrayString/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ArrayString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Euroopa üks pealinnadest on ";
+            Console.WriteLine("Tuple");
+
+            //näide 1
+            var cities = Tuple.Create("Tallinn", "Pariis", "Rooma", "London", "Helsingi");
+            //tuple all on 5 erinevat vaartust ja selleparast saab antud
+            //juhul5 item-t sisestada
+            Console.WriteLine(sentence + cities.Item5);
+            Console.WriteLine("---------------------");
+
+            //näide 2
+            string[] city = { "Tallinn", "Pariis", "Rooma", "London", "Helsingi" };
+            for (int i = 0; i < city.Length; i++) 
+            {
+                Console.WriteLine(sentence + city[i]);
+            }
+            Console.WriteLine("--------------------");
+
+            //näide 3
+            Console.WriteLine(sentence + city[2]);
+            Console.WriteLine("----------------");
+
+            //näide 4
+            Random number = new Random();
+            int randomNumber = number.Next(0, 4);
+            Console.WriteLine(sentence + city[randomNumber]);
+        }
+    }
+}
---
+++ KITKAT/KITKAT/Metodcall/Metodcall/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Metodcall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Console.WriteLine("Hello, World!");
+            SayHello();
+            SayGoodbye();
+
+        }
+     
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello");
+
+        }
+
+        static void SayGoodbye()
+        {
+            Console.WriteLine("goodbye");
+        }
+        
+        
+    }
+}
