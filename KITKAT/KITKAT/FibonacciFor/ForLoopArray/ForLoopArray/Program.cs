90dc1be4ec391f292e5c2b931f821db0128d046c
Opilane TTHK <opilane@tthk.ee>
2026-01-05 09:44:47 +0200

    hello

---
+++ KITKAT/KITKAT/FibonacciFor/ForLoopArray/ForLoopArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ForLoopArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //vaja teha string array H, e, l, l, o
+            //kasuta for loopi
+
+            string[] letters = { "H", "e", "l", "l", "o" };
+            for (int i = 0; i < letters.Length; i++)
+            {
+                Console.WriteLine(letters[i]);
+            }
+            string[] array = { "H", "e", "l", "l", "o" };
+            foreach (string i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
