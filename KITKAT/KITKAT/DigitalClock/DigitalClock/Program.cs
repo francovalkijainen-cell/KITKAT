924711c29bed36600143f9ca91d3ff3c6ebe82a4
Opilane TTHK <opilane@tthk.ee>
2026-01-09 09:33:00 +0200

    ggg

---
+++ KITKAT/KITKAT/DigitalClock/DigitalClock/Program.cs
@@ -0,0 +1,59 @@
+﻿
+namespace DigitalClock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Digital Clock";
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                // static method clears the console if possible.
+                Console.Clear();
+                //Converts the value of the current DateTime object
+                //to its equivalent string representation.
+                
+                string time = DateTime.Now.ToString("HH:mm:ss");
+                //calculates the remaining space (in characters) in the
+                //console window after accounting for the length of a time string
+               
+                int x = (Console.WindowWidth - time.Length) / 2;
+                //Gets or sets the height of the console window area.
+                int y = Console.WindowHeight / 2;
+                //Sets the position of the cursor.
+                Console.SetCursorPosition(x, y);
+                //Writes the text representation of the specified value
+                //or values to the standard output stream.
+                Console.Write(time);
+                //Suspends the current thread for the specified amount of time.
+                Thread.Sleep(1000);
+
+                int main()
+                {
+                    int hour, min, sec;
+
+                    // This loop for hours
+                    for (hour = 12; hour < 24; hour++)
+                    {
+
+                        // This loop for minutes
+                        for (min = 0; min < 60; min++)
+                        {
+
+                            // This loop for seconds
+                            for (sec = 0; sec < 60; sec++)
+                            {
+                               
+                              
+                            }
+                        }
+                    }
+                    return 0;
+                }
+
+            }
+        }
+    }
+}
