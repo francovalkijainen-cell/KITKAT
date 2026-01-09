4c10b9e6d451e3e8876d301eee957ae8a699557b
Opilane TTHK <opilane@tthk.ee>
2026-01-09 11:05:38 +0200

    ruut

---
+++ KITKAT/KITKAT/Ruut/Ruut/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Ruut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //teha for loopiga ruut
+            int size;
+
+            size = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Sisestage ruudu külje pikkus");
+
+            double sideLength = Convert.ToDouble(Console.ReadLine());
+
+
+            double area = sideLength * sideLength;
+
+
+            double circumference = 4 * sideLength;
+
+            Console.WriteLine($"Ruudu pindala on: {area}");
+            Console.WriteLine($"Ruudu ümbermõõt on: {circumference}");
+
+            Console.ReadKey();
+        }
+    }
+}
+
