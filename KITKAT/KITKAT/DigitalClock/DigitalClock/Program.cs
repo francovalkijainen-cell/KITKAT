b5d5322422041be73414c5b011504de2a29ab8eb
Opilane TTHK <opilane@tthk.ee>
2026-01-09 09:41:56 +0200

    Update Program.cs

--- KITKAT/KITKAT/DigitalClock/DigitalClock/Program.cs
+++ KITKAT/KITKAT/DigitalClock/DigitalClock/Program.cs
@@ -10,49 +10,32 @@ namespace DigitalClock
 
             while (true)
             {
-                // static method clears the console if possible.
-                Console.Clear();
-                //Converts the value of the current DateTime object
-                //to its equivalent string representation.
-                
-                string time = DateTime.Now.ToString("HH:mm:ss");
-                //calculates the remaining space (in characters) in the
-                //console window after accounting for the length of a time string
-               
-                int x = (Console.WindowWidth - time.Length) / 2;
-                //Gets or sets the height of the console window area.
-                int y = Console.WindowHeight / 2;
-                //Sets the position of the cursor.
-                Console.SetCursorPosition(x, y);
-                //Writes the text representation of the specified value
-                //or values to the standard output stream.
-                Console.Write(time);
-                //Suspends the current thread for the specified amount of time.
-                Thread.Sleep(1000);
+                //// static method clears the console if possible.
+                //Console.Clear();
+                ////Converts the value of the current DateTime object
+                ////to its equivalent string representation.
 
-                int main()
+                //string time = DateTime.Now.ToString("HH:mm:ss");
+                ////calculates the remaining space (in characters) in the
+                ////console window after accounting for the length of a time string
+
+                //int x = (Console.WindowWidth - time.Length) / 2;
+                ////Gets or sets the height of the console window area.
+                //int y = Console.WindowHeight / 2;
+                ////Sets the position of the cursor.
+                //Console.SetCursorPosition(x, y);
+                ////Writes the text representation of the specified value
+                ////or values to the standard output stream.
+                //Console.Write(time);
+                ////Suspends the current thread for the specified amount of time.
+                //Thread.Sleep(1000);
+                for (; ; )
                 {
-                    int hour, min, sec;
-
-                    // This loop for hours
-                    for (hour = 12; hour < 24; hour++)
-                    {
-
-                        // This loop for minutes
-                        for (min = 0; min < 60; min++)
-                        {
-
-                            // This loop for seconds
-                            for (sec = 0; sec < 60; sec++)
-                            {
-                               
-                              
-                            }
-                        }
-                    }
-                    return 0;
+                    Console.Clear();
+                    Console.WriteLine("Digital Clock\n");
+                    Console.WriteLine(DateTime.Now.ToString("HH:mm:ss"));
+                    Thread.Sleep(1000);
                 }
-
             }
         }
     }
