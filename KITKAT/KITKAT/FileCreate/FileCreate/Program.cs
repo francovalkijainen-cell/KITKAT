aad5b77c03d043062543fae1da4691473f13fd9f
Opilane TTHK <opilane@tthk.ee>
2026-01-16 10:43:45 +0200

    filecreate

---
+++ KITKAT/KITKAT/FileCreate/FileCreate/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FileCreate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjutab faili läbi konsooli");
+
+            string failpath = "C:/Users/opilane/Desktop/WriteToFile.txt";
+            string input = Console.ReadLine();
+
+            File.WriteAllText(failpath, input);
+        }
+    }
+}
